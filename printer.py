2195537a44c8a3d9f0ed28f185db0769d9ce45be
KOSM1K <rob.357.ot@gmail.com>
2022-11-22 19:15:56 +0300

    colorful printing

---
+++ printer.py
@@ -0,0 +1,15 @@
+colors = {
+    'bas': '\033[0m',
+    'gre': '\033[32m',
+    'yel': '\033[33m',
+    'red': '\033[31m'
+}
+
+
+def printCLR(color, text):
+    print(colors[color], text, colors['bas'], sep='', end='')
+
+
+if __name__ == '__main__':
+    printCLR('gre', '[I]')
+    print(' something', end='')
